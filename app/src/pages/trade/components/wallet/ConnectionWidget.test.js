7bc6cceb9aa524606b16d719985aa8d3dc0e4050
Maksim Lunochkin <maxlun86@gmail.com>
2018-10-26 22:59:38 +0300

    Replace mocha with tape on frontend.

--- app/src/pages/trade/components/wallet/ConnectionWidget.test.js
+++ app/src/pages/trade/components/wallet/ConnectionWidget.test.js
@@ -1,3 +1,4 @@
+import test from 'tape-promise/tape'
 import React from 'react'
 import ConnectionWidget from './ConnectionWidget'
 import Enzyme, { mount } from 'enzyme'
@@ -8,64 +9,57 @@ import thunk from 'redux-thunk'
 import store from 'store'
 import Web3Context from 'contexts/Web3Context'
 import Web3 from 'web3'
-import { expect } from 'chai'
 import moxios from 'moxios'
 const ganache = require('ganache-cli')
 
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 
-/* eslint-env jest */
-
 Enzyme.configure({ adapter: new Adapter() })
 
-describe('ConnectionWidget', () => {
-  beforeEach(() => {
+const wrappedTest = (title, cb) =>
+  test(`ConnectionWidget | ${title}`, async t => {
     // import and pass your custom axios instance to this method
     moxios.install()
-  })
 
-  afterEach(() => {
+    await cb(t)
+
     // import and pass your custom axios instance to this method
     moxios.uninstall()
   })
 
-  it('should render metamask link', () => {
-    const mockStore = configureMockStore([
-      thunk
-    ])
+wrappedTest('should render metamask link', t => {
+  const mockStore = configureMockStore([
+    thunk
+  ])
 
-    const store = mockStore()
+  const store = mockStore()
 
-    const wrapper = mount(<Provider store={store}><ConnectionWidget /></Provider>)
-    expect(
-      wrapper.find('[href="https://metamask.io/"]').exists()
-    ).to.equal(true)
+  const wrapper = mount(<Provider store={store}><ConnectionWidget /></Provider>)
 
-    wrapper.unmount()
-  })
+  t.ok(wrapper.find('[href="https://metamask.io/"]').exists())
 
-  it('should render etherscan link', async () => {
-    const web3 = new Web3(ganache.provider({
-      accounts: [
-        { balance: 0 }
-      ]
-    }))
+  wrapper.unmount()
+})
 
-    const wrapper = mount(
-      <Provider store={store}>
-        <Web3Context.Provider value={web3}>
-          <ConnectionWidget />
-        </Web3Context.Provider>
-      </Provider>
-    )
+wrappedTest('should render etherscan link', async t => {
+  const web3 = new Web3(ganache.provider({
+    accounts: [
+      { balance: 0 }
+    ]
+  }))
 
-    await delay(200)
-    wrapper.update()
+  const wrapper = mount(
+    <Provider store={store}>
+      <Web3Context.Provider value={web3}>
+        <ConnectionWidget />
+      </Web3Context.Provider>
+    </Provider>
+  )
 
-    expect(
-      wrapper.find('EtherscanLink').exists()
-    ).to.equal(true)
+  await delay(200)
+  wrapper.update()
 
-    wrapper.unmount()
-  })
+  t.ok(wrapper.find('EtherscanLink').exists())
+
+  wrapper.unmount()
 })
