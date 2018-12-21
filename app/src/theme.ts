1dcc081c233177fc1e890b84f5cbd76bcac5a2b6
Mikhail Kupriianov <kostromich@inbox.ru>
2018-12-21 15:51:34 +0300

    Add bid and ask colors to palette. Apply those colors to LimitOrderForm buttons.

--- app/src/theme.ts
+++ app/src/theme.ts
@@ -7,7 +7,21 @@ const theme = createMuiTheme({
   },
   palette: {
     primary: grey
+  },
+  custom: {
+    bidColor: {
+      main: '#990000',
+      light: '#C20000',
+      dark: '#700000',
+      contrastText: '#fff'
+    },
+    askColor: {
+      main: '#009944',
+      light: '#00C545',
+      dark: '#007037',
+      contrastText: '#fff'
+    }
   }
-})
+} as any)
 
 export default theme
