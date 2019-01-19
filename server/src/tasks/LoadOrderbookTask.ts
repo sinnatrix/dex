392e8056eee8432fce062e33f11d81986634e8a7
Maksim Lunochkin <maxlun86@gmail.com>
2019-01-19 06:31:33 +0300

    Fix orders insert ignore mechanic.

--- server/src/tasks/LoadOrderbookTask.ts
+++ server/src/tasks/LoadOrderbookTask.ts
@@ -1,11 +1,13 @@
 import { getRepository, getCustomRepository } from 'typeorm'
 import RelayerEntity from '../entities/Relayer'
+import { IHttpRelayer } from '../types'
 import AssetPairEntity from '../entities/AssetPair'
 import OrderRepository from '../repositories/OrderRepository'
+import RelayerService from '../services/RelayerService'
 const ZeroEx = require('0x.js')
 
 class LoadOrderbookTask {
-  relayerService: any
+  relayerService: RelayerService
 
   constructor ({ relayerService }) {
     this.relayerService = relayerService
@@ -20,12 +22,12 @@ class LoadOrderbookTask {
 
     const tokenPair: any = await getRepository(AssetPairEntity).findOne()
 
-    const orderbook = await this.relayerService.loadOrderbook(relayer, {
+    const orderbook = await this.relayerService.loadOrderbook(relayer as IHttpRelayer, {
       baseAssetAddress: tokenPair.tokenAAddress,
       quoteAssetAddress: tokenPair.tokenBAddress
     })
 
-    const orders = orderbook.bids.concat(orderbook.asks)
+    const orders = orderbook.bids.records.concat(orderbook.asks.records)
 
     const orderRepository = getCustomRepository(OrderRepository)
 
